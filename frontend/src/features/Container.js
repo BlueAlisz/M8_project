8e2eced910c42092c5a617c8d0b0c350ea93d049
BlueAlisz <nattaphat_y@cmu.ac.th>
2021-10-23 00:23:43 +0700

    create Container

---
+++ frontend/src/features/Container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+function Container({ children, className }) {
+  return <main className={className}>{children}</main>;
+}
+
+export default styled(Container)`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 4rem 0;
+`;
